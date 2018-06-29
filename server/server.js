8e65dad9928bc94b17e5550ef99d3deb93a3c125
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2018-06-30 09:21:24 +1000

    Introduced socket.io Wrote testing code for socket.io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-var app = express();
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	socket.on('createMessage', (message) => {
+		console.log('create message', message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
+	});
+
+	socket.on('disconnect', () => {
+		console.log('User was disconnected');
+	});
+});
+
+server.listen(port, () => {
 	console.log(`Server is up on ${port}`);
 });
 
