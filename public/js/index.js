9ba28e429263d1784f4347138ad0432338618934
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2018-06-30 14:23:10 +1000

    Introduced mustache Updated the view

--- public/js/index.js
+++ public/js/index.js
@@ -9,21 +9,27 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-	var li = $('<li></li>');
-	li.text(`${message.from}: ${message.text}`);
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = $('#message-template').html();
+	var html = Mustache.render(template, {
+		text: message.text,
+		from: message.from,
+		createdAt: formattedTime
+	});
 
-	$('#messages').append(li);
+	$('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
-	var li = $('<li></li>');
-	var a = $('<a target="_blank">My current location</a>');
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = $('#location-message-template').html();
+	var html = Mustache.render(template, {
+		url: message.url,
+		from: message.from,
+		createdAt: formattedTime
+	});
 
-	li.text(`${message.from}: `);
-	a.attr('href', message.url);
-	li.append(a);
-
-	$('#messages').append(li);
+	$('#messages').append(html);
 });
 
 socket.on('createMessage', function(message) {
