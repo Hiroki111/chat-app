886c8e7353abda7710716b3acc74ec9864cb5049
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2018-06-30 13:34:11 +1000

    Updated the server and message util Updated html and css

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,19 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-	console.log('newMessage', message);
 	var li = $('<li></li>');
 	li.text(`${message.from}: ${message.text}`);
+
+	$('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function(message) {
+	var li = $('<li></li>');
+	var a = $('<a target="_blank">My current location</a>');
+
+	li.text(`${message.from}: `);
+	a.attr('href', message.url);
+	li.append(a);
 
 	$('#messages').append(li);
 });
@@ -20,20 +30,36 @@ socket.on('createMessage', function(message) {
 	console.log('createMessage', message);
 });
 
-socket.emit('createMessage', {
-	from: 'Hiroki',
-	text: 'Hey'
-}, function(data) {
-	console.log('From Server :', data);
-});
-
 $('#message-form').on('submit', function(e) {
 	e.preventDefault();
 
+	var messageTextBox = $('[name=message]');
+
 	socket.emit('createMessage', {
 		from: "User",
-		text: $('[name=message]').val()
+		text: messageTextBox.val()
 	}, function() {
+		messageTextBox.val('');
+	});
+});
 
+var locationButton = $('#send-location');
+
+locationButton.on('click', function() {
+	if (!navigator.geolocation) {
+		return alert('Geolocation not supported by your browser.');
+	}
+
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+	navigator.geolocation.getCurrentPosition(function(position) {
+		locationButton.removeAttr('disabled').text('Send location');
+		socket.emit('createLocationMessage', {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		});
+	}, function() {
+		locationButton.removeAttr('disabled').text('Send location');
+		alert('Unable to fetch location.');
 	});
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate correct message object', () => {
@@ -10,5 +10,20 @@ describe('generateMessage', () => {
 			text: "Hey what\'s up"
 		});
 		expect(typeof message.createdAt).toBe('number');
+	});
+})
+
+describe('generateLocationMessage', () => {
+
+	it('should generate correct location object', () => {
+		var from = 'Hiroki';
+		var lat = '-20';
+		var lon = '155';
+		var location = generateLocationMessage(from, lat, lon);
+		expect(location).toMatchObject({
+			from,
+			url: `https://www.google.com.au/maps?q=${lat},${lon}`
+		});
+		expect(typeof location.createdAt).toBe('number');
 	});
 })
